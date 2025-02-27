988887b573f62fdeaafd1412e40ba750ac674b29
Luis Alvarez <alvarez.en.luis@gmail.com>
2025-02-26 23:55:09 -0800

    working on C# code for RabbitMQ

--- Ch2MessagePatterns/RabbitMQSetup/Sender.cs
+++ Ch2MessagePatterns/RabbitMQSetup/Sender.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 using System.Text;
 
@@ -5,8 +7,18 @@ public class Sender {
   private readonly static string QUEUE_NAME = "event_queue";
   private readonly static string DEFAULT_EXCHANGE = "";
 
+  private readonly ILogger<Sender> _logger = null!;
+  private readonly IConfiguration _configuration = null!;
+
   private IConnection? _connection;
   private IChannel? _channel;
+
+  public Sender(){}
+
+  public Sender(ILogger<Sender> logger, IConfiguration configuration) {
+    _logger = logger;
+    _configuration = configuration;
+  }
 
   public async void initialize()
   {
@@ -18,11 +30,63 @@ public class Sender {
       };
       _connection = await factory.CreateConnectionAsync();
       _channel = await _connection.CreateChannelAsync();
+
+      await _channel!.QueueDeclareAsync(queue: QUEUE_NAME, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+
     }
     catch (System.Exception ex)
     {
       throw ex;
     }
   }
+
+  public async void send(string message){
+    try
+    {
+      byte[] body = Encoding.UTF8.GetBytes(message);
+      await _channel!.BasicPublishAsync(exchange: DEFAULT_EXCHANGE, routingKey: QUEUE_NAME, body: body);
+    }
+    catch (Exception ex)
+    {
+      throw ex;
+    }
+  }
+
+  public async void send(string exchange, string type, string message)
+  {
+    try
+    {
+      byte[] body = Encoding.UTF8.GetBytes(message);
+      await _channel!.ExchangeDeclareAsync(exchange: exchange, type: type);
+      await _channel!.BasicPublishAsync(exchange: exchange, routingKey: "", body: body);
+    }
+    catch (Exception ex)
+    {
+      throw ex;
+    }
+  }
+
+  public void destroy()
+  {
+    try
+    {
+      if (_channel != null 
+        && _connection != null
+        && _channel.IsOpen)
+      {
+        _channel.CloseAsync();
+        _channel.Dispose();
+        _connection.CloseAsync();
+        _connection.Dispose();
+      }
+    }
+    catch (Exception ex)
+    {
+      throw ex;
+    }
+  }
+
+
 
 }
