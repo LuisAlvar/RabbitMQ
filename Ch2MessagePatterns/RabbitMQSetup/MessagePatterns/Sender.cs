84a84b66f31ce1b1af761e0e874c2863d5a5c202
luis <alvarez.en.luis@gmail.com>
2025-03-03 00:07:08 -0800

    this working on the req-respo communication type

--- Ch2MessagePatterns/RabbitMQSetup/MessagePatterns/Sender.cs
+++ Ch2MessagePatterns/RabbitMQSetup/MessagePatterns/Sender.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.Retry;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Exceptions;
 using System.Data.Common;
 using System.Net.Sockets;
@@ -16,6 +17,9 @@ public class Sender
 {
   private const string QUEUE_NAME = "event_queue";
   private const string DEFAULT_EXCHANGE = "";
+  private const string REQUEST_QUEUE = "request_queue";
+  private const string RESPONSE_QUEUE = "response_queue";
+
   private const string HOST_NAME = "localhost";
   private const int HOST_PORT = 5672;
 
@@ -108,6 +112,84 @@ public class Sender
       return false;
     }
     return true;
+  }
+
+
+  /// <summary>
+  /// Appropriate for Request-reply communication
+  /// </summary>
+  /// <param name="requestqueue"></param>
+  /// <param name="message"></param>
+  /// <param name="correlationId"></param>
+  /// <returns></returns>
+  public bool SendRequest(string requestqueue, string message, string correlationId)
+  {
+    try
+    {
+      var body = Encoding.UTF8.GetBytes(message);
+
+      _channel.QueueDeclare(queue: REQUEST_QUEUE, durable: false, exclusive: false, autoDelete: false, arguments: null);
+      _channel.QueueDeclare(queue: RESPONSE_QUEUE, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+      var properties = _channel.CreateBasicProperties();
+      properties.CorrelationId = correlationId.ToString();
+      properties.ReplyTo = "RESPONSE_QUEUE";
+
+      _channel.BasicPublish(exchange: DEFAULT_EXCHANGE, routingKey: REQUEST_QUEUE, mandatory: false, basicProperties: properties, body: body);
+      return true;
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.ToString());
+      return false;
+    }
+  }
+
+  /// <summary>
+  /// 
+  /// </summary>
+  /// <returns></returns>
+  public string WaitForResponse(string correlationId)
+  {
+    EventingBasicConsumer consumer = new EventingBasicConsumer(_channel);
+    string result = string.Empty;
+
+    try
+    {
+      _channel.BasicConsume(queue: RESPONSE_QUEUE, autoAck: false, consumer);
+      consumer.Received += (model, ea) =>
+      {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        var props = ea.BasicProperties;
+        if (props != null)
+        {
+          var msgCorrelationId = props.CorrelationId;
+          if (!correlationId.Equals(msgCorrelationId))
+          {
+            Console.WriteLine("Received response of another request.");
+          }
+          else
+          {
+            Console.WriteLine($"Message received: {message}");
+            result = message;
+          }
+        }
+      };
+    }
+    catch (IOException ex)
+    {
+      Console.WriteLine(ex.ToString());
+    }
+    catch (RabbitMQ.Client.Exceptions.OperationInterruptedException ex)
+    {
+      Console.WriteLine(ex.ToString());
+    }
+    catch (Exception ex)
+    {
+      Console.WriteLine(ex.ToString());
+    }
+    return result;
   }
 
   /// <summary>
--- Ch2MessagePatterns/RabbitMQSetup/Program.cs
+++ Ch2MessagePatterns/RabbitMQSetup/Program.cs
@@ -2,12 +2,13 @@
 using RabbitMQSetup.Demo;
 Console.WriteLine("RabbitMQSetup");
 
+# region Point-to-Point 
 //DefaultExchangeSenderDemo.Main(args);
 //Console.Write("Wait.... (main app thread sleeping) ... ");
 //Task.Delay(2000).Wait();
 //Console.WriteLine("Done");
 //await CompetingReceiverDemo.MainFCFS(args);
-
+# endregion 
 
 FanoutExchangeSenderDemo.Main(args);
 Console.Write("Wait.... (main app thread sleeping) ... ");
